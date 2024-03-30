fa621094c4bf6ce0e840802aee34905c4e2d4493
brootul <brootul@yahoo.com>
2024-03-30 08:48:36 -0400

    changed the name to be all lower case

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 def main():
-    with open("books/Frankenstein.txt") as f:
-        print(f.read())
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    print(f"{num_words} words found in the document")
 
-main()list
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
