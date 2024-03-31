e7e8ca5cd915e1271e02db0ad8a46daf96fda93c
brootul <brootul@yahoo.com>
2024-03-31 11:02:32 -0400

    added a funtion to convert all letters to lower case and print a count of each letter

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    letter_count = get_letter_count(text)
     print(f"{num_words} words found in the document")
+    print(f"{letter_count}'s found in the document")
 
 
 def get_num_words(text):
@@ -13,6 +15,17 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+
+def get_letter_count(text):
+    letters = text.lower()
+    counts = {}
+    for letter in letters:
+        if letter not in counts:
+            counts[letter] = 0
+        counts[letter] += 1
+    return counts
+        
 
 
 main()
